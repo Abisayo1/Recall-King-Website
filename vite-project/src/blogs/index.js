fe2b4d58b15d485c0be4de7b0bbc02b786e54bab
Lizzy909 <silveyelizabeth13@gmail.com>
2025-12-16 13:28:54 +0100

    I just added a new blog to the site titled one_scan_save_your_day

--- vite-project/src/blogs/index.js
+++ vite-project/src/blogs/index.js
@@ -2,9 +2,10 @@ import recallKing from "./why-we-created-recall-king";
 import dangersOfRecalledProducts from "./dangers-of-recalled-products";
 import spotRecalled from "./spot_recalled.jsx";
 import loyalCustomers from "./loyal_customers.jsx"
+import scanSave from "./one_scan_save_your_day.jsx"
 // import howRecallsWork from "./how-recalls-work";
 // Add more imports here as you create new blog files
 
-const blogs = [recallKing, dangersOfRecalledProducts, spotRecalled, loyalCustomers];
+const blogs = [recallKing, dangersOfRecalledProducts, spotRecalled, loyalCustomers, scanSave];
 
 export default blogs;
---
+++ vite-project/src/blogs/one_scan_save_your_day.jsx
@@ -0,0 +1,42 @@
+const blog = {
+  id: 5,
+  title: "Stay Ahead Don't feel the Bruise: How one scan can save your day",
+  slug: "scan_save_your_day",
+  headerImage: "../recalled.png",
+  content: `
+<p>In today's world, shopping is easier than ever. With just a tap or a click, your desired product
+is on its way to your doorstep. But what happens when the product you've eagerly awaited
+has been recalled? Imagine if your favorite snack, that trending skincare cream, or your
+child's new toy was quietly removed from shelves, and you had no clue.
+</p><br/>
+
+<strong>Meet Recall King</strong>
+
+<p><br>Enter Recall King, your personal safety sidekick. This isn't just another app; it's a
+revolutionary tool designed to keep you informed and protected. With one simple barcode
+scan, Recall King cross-references the product against a comprehensive, live database of
+official recalls. If your product is safe, you’re in the clear. If it’s not, you’ll receive an instant
+alert accompanied by clear instructions on what to do next.
+No more guessing. No more doomscrolling.</p><br />
+
+<strong>Comprehensive Coverage
+</strong>
+<p><br>From baby products to skincare, food items to electronics, Recall King has it all covered. It's
+smart, simple, and lightning-fast. Plus, you don't need to be a tech wizard to use it. Just scan,
+and your safety is assured.
+Why let a bad product play you?
+When you can scan first, stay safe, and protect those you love?</p><br />
+
+<strong>Shop Smarter</strong><br />
+<p>So, next time you're shopping, whether in-store or online, remember this mantra:
+Stay ahead, don’t feel the bruise. Recall King is here to help you choose.
+</br><br />
+With Recall King, you can shop with confidence, knowing you have the power to make
+informed decisions and keep your loved ones secure.</p>
+
+
+  `,
+  inlineImages: ["/recallHero.png"],
+};
+
+export default blog;
