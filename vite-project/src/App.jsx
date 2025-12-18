f13458018620677ea5cd5a090f9bb970bcca4d92
Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>
2025-12-18 01:20:22 +0100

    I added deeplink logic
    Signed-off-by: Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home.jsx';
-import HowitWorks from './components/HowitWorks';
-import Features from './components/Features';
-import FAQ from './components/FAQ';
-import Footer from './components/Footer';
-import BlogPage from './BlogPage.jsx';
-import BlogDetails from './BlogDetails.jsx'; // ✅ NEW
-import PrivacyPolicy from './PrivacyPolicy.jsx';
-import TermsAndConditions from './TermsAndConditions.jsx';
-import RedirectPage from './RedirectPage.jsx';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home.jsx";
+import HowitWorks from "./components/HowitWorks";
+import Features from "./components/Features";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
+import BlogPage from "./BlogPage.jsx";
+import BlogDetails from "./BlogDetails.jsx";
+import PrivacyPolicy from "./PrivacyPolicy.jsx";
+import TermsAndConditions from "./TermsAndConditions.jsx";
+import RedirectPage from "./pages/OpenRedirect.jsx";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <div className="pt-20 scroll-smooth">
         <Routes>
@@ -31,13 +31,14 @@ function App() {
             }
           />
           <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:slug" element={<BlogDetails />} /> {/* ✅ NEW */}
+          <Route path="/blog/:slug" element={<BlogDetails />} />
           <Route path="/terms" element={<TermsAndConditions />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/download" element={<RedirectPage />} />
+          <Route path="/open/*" element={<RedirectPage />} />
         </Routes>
       </div>
-    </Router>
+    </>
   );
 }
 
