2044f75c9658dea8dca229cc181512e80603cf69
Lizzy909 <silveyelizabeth13@gmail.com>
2026-01-08 14:22:14 +0100

    Update blog content

--- vite-project/src/blogs/index.js
+++ vite-project/src/blogs/index.js
@@ -4,6 +4,7 @@ import spotRecalled from "./spot_recalled.jsx";
 import loyalCustomers from "./loyal_customers.jsx";
 import scanSave from "./one_scan_save_your_day.jsx";
 import Scan1 from "./Scan1.jsx";
+import Dogs from "./Dogs Treats Recalled.Jsx";
 
 // Add more imports here as you create new blog files
 
@@ -13,7 +14,8 @@ const blogs = [
   spotRecalled,
   loyalCustomers,
   scanSave,
-  Scan1
+  Scan1,
+ Dogs,
 ];
 
 export default blogs;
